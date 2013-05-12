2e7ffeb577fd90ac59c648a8dfa0cf5d5c3287f9
Maciej Brencz <maciej.brencz@gmail.com>
2013-05-12 17:35:31 +0200

    Use EPUB as a base format

--- convert.pl
+++ convert.pl
@@ -1,17 +1,21 @@
 #!/usr/bin/env perl
 use common::sense;
 use LWP::UserAgent;
-use URI::Escape;
 use HTML::TreeBuilder::XPath;
 use YAML::Tiny;
 use Data::Dumper;
 use Cwd 'realpath';
 use File::Basename 'dirname';
-use EBook::MOBI;
+use EBook::EPUB;
 use Encode;
 
 # HTTP utils
-use constant USER_AGENT => "Mozilla/5.0 (wikisource2mobi)";
+use constant {
+	VERSION    => "0.0.1",
+	USER_AGENT => "Mozilla/5.0 (wikisource2mobi)"
+};
+
+say "wikisource2mobi v" . VERSION;
 
 sub getUrl($) {
 	(my $url) = @_;
@@ -27,8 +31,32 @@ sub getUrl($) {
 	return Encode::decode('utf8', $html);
 }
 
-# check dependencies
-die "Please install ebook-meta" unless system("which ebook-meta > /dev/null") == 0;
+# chapters generation
+my $chaptersCnt;
+sub addChapter($$$) {
+	use File::Temp qw/ :POSIX /;
+	my ($epub, $title, $html) = @_;
+
+	# generate temporary XHTML file
+	my $file = tmpnam();
+
+	# create temporary file
+	open my $fp, '>:utf8', $file or die "Cannot create temporary file";
+	print $fp $html;
+	close $fp;
+
+	# add to the TOC
+	$chaptersCnt++;
+	my $name = "chapter$chaptersCnt.xhtml";
+	my $chapter_id = $epub->copy_xhtml($file, $name);
+
+	$epub->add_navpoint(
+		label       => $title,
+		id          => $chapter_id,
+		content     => $name,
+		play_order  => $chaptersCnt
+	);
+}
 
 # validate CLI arguments
 die "Please pass book YAML file" unless defined $ARGV[0];
@@ -48,23 +76,13 @@ $yaml = $yaml->[0];
 say "My workplace will be in $workDir...";
 
 # prepare the book
-my $book = EBook::MOBI->new();
-my $converter = EBook::MOBI::Converter->new();
+my $epub = EBook::EPUB->new;
 
-$book->set_filename("$workDir/book.mobi");
-$book->set_encoding(':encoding(UTF-8)');
-$book->set_title   (Encode::encode('utf8', $yaml->{title}));
-$book->set_author  (Encode::encode('utf8', $yaml->{author}));
-
-# generate cover
-$book->add_mhtml_content( $converter->paragraph("<br /><br />") );
-$book->add_mhtml_content("<h1><center>$yaml->{title}</center></h1>");
-$book->add_mhtml_content("<h2><center>$yaml->{author}</center></h2>");
-$book->add_pagebreak();
-
-# TOC
-$book->add_toc_once("Spis treści");
-$book->add_pagebreak();
+$epub->add_title($yaml->{title});
+$epub->add_author($yaml->{author});
+$epub->add_language('pl');
+$epub->add_publisher('wikisource2mobi');
+$epub->add_source($yaml->{source});
 
 # fetch the index file
 my $source = $yaml->{source} . "?action=raw";
@@ -96,6 +114,14 @@ say "\nFound " . scalar(@chapters) . " chapters:";
 
 #say Dumper($index); say Dumper(@chapters); exit;
 
+# cover
+addChapter($epub, "Okładka", <<COVER
+	<p><br /><br /></p>
+	<h1><center>$yaml->{title}</center></h1>
+	<h2><center>$yaml->{author}</center></h2>
+COVER
+);
+
 # fetch chapters
 foreach my $chapter (@chapters) {
 	my $url = "http://pl.wikisource.org/w/index.php?title=" . $chapter . "&action=render";
@@ -112,27 +138,32 @@ foreach my $chapter (@chapters) {
 	# add chapter data
 	$chapter =~ tr/\_/ /;
 	$chapter =~ s/\s?\((.*)$//;
-	$book->add_mhtml_content( $converter->title($chapter) ); # add a chapter name
-	$book->add_mhtml_content( $converter->paragraph("<br /><br />") );
 
-	foreach(@nodes) {
+	# chapter title
+	my $content;
+	$content .= "<p><br /><br /></p>\n";
+	$content .=  "<h1>$chapter</h1>\n";
+	$content .= "<p><br /><br /></p>\n";
+
+	# content
+	foreach (@nodes) {
 		s/\[\d+\]//g; # remove references
 		next if /^\s?$/; # skip empty lines
 
-		$book->add_mhtml_content( $converter->paragraph($_) ); # add a paragraph
+		$content .= "<p>$_</p>\n";
 	}
 
-	$book->add_pagebreak();
+	addChapter($epub, $chapter, $content);
 }
 
 # copyright stuff
 my $date = `date +%d\\ %B\\ %Y`;
-$book->add_mhtml_content(<<COPYRIGHT
+addChapter($epub, "Nota redakcyjna", <<COPYRIGHT
 <p><center><small>
 	<br /><br />
 	<br /><br />
 	<br /><br />
-	Przygotowanie oraz konwersja do formatu MOBI:
+	Przygotowanie oraz konwersja do formatów EPUB i MOBI:
 	<br />
 	Maciej Brencz &lt;maciej.brencz\@gmail.com&gt;
 	<br /><br />
@@ -150,7 +181,7 @@ $book->add_mhtml_content(<<COPYRIGHT
 	<br>
 	$date r.
 	<br /><br />
-	Generowanie plików MOBI napędza <a href="https://github.com/macbre/wikisource2mobi">wikisource2mobi</a>
+	Generowanie plików EPUB oraz MOBI napędza <a href="https://github.com/macbre/wikisource2mobi">wikisource2mobi</a> v@{[VERSION]}
 	<br /><br />
 	<strong>Miłej lektury!</strong>
 </small></center></p>
@@ -158,19 +189,13 @@ COPYRIGHT
 );
 
 # now generate an ebook
-say "\nWriting MOBI file...";
-$book->make();
+say "\nWriting EPUB file...";
+$epub->pack_zip("$workDir/book.epub");
 
-# generate HTML file with the content
-open my $html, '>:encoding(UTF-8)', "$workDir/content.html" or die "Cannot create HTML file";
-print $html $book->print_mhtml(1);
-close $html;
-
-# save the file
-$book->save() or die "save() failed";
-
-# set meta data
-say "\nWriting meta data....";
-system("/usr/bin/env ebook-meta --language pl --book-producer wikisource2mobi $workDir/book.mobi > /dev/null") == 0 or die ("Cannot modify ebook's meta data");
+# convert to mobi as well
+# @see http://manual.calibre-ebook.com/cli/ebook-convert.html
+say "Generating MOBI file from EPUB...";
+system("/usr/bin/env ebook-convert $workDir/book.epub $workDir/book.mobi > /dev/null") == 0 or say("Conversion failed");
 
 say "Done!";
+
